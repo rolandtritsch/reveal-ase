35de4fa1af2d314dbaee4bcda4fd854598ae18c3
Roland Tritsch <roland@tritsch.email>
2025-03-17 12:00:38 +0000

    002: WIP: Cleanup/Checkpoint

--- 002/010/typescript-fib/fibonacci.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.fibonacci = void 0;
-function fibonacci(n) {
-    if (n <= 1)
-        return n;
-    return fibonacci(n - 1) + fibonacci(n - 2);
-}
-exports.fibonacci = fibonacci;
